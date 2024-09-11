357453942ab1b5fa8ca932103f3617542a0e11c3
everydayspring <lee.bom.952@gmail.com>
2024-09-11 17:24:08 +0900

    레벨 3-10: AuthServiceTest - 로그인_정상동작()

--- src/test/java/org/example/expert/domain/auth/service/AuthServiceTest.java
+++ src/test/java/org/example/expert/domain/auth/service/AuthServiceTest.java
@@ -2,18 +2,23 @@ package org.example.expert.domain.auth.service;
 
 import org.example.expert.config.JwtUtil;
 import org.example.expert.config.PasswordEncoder;
+import org.example.expert.domain.auth.dto.request.SigninRequest;
 import org.example.expert.domain.auth.dto.request.SignupRequest;
+import org.example.expert.domain.auth.dto.response.SigninResponse;
 import org.example.expert.domain.auth.dto.response.SignupResponse;
 import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.user.entity.User;
 import org.example.expert.domain.user.enums.UserRole;
 import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -34,59 +39,93 @@ public class AuthServiceTest {
     @InjectMocks
     private AuthService authService;
 
-    @Test
-    void 회원가입_정상_동작() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("email", "pwd", "USER");
+    @Nested
+    class SignupTest {
+        @Test
+        void 회원가입_정상동작() {
+            // given
+            SignupRequest signupRequest = new SignupRequest("email", "pwd", "USER");
 
-        User user = new User("email", "$2a$04$jfQeXoc7b5IWWvZFPDE.he56RmITYyjnPA4haWZB2EgFda9uDXsHC", UserRole.USER);
+            User user = new User("email", "$2a$04$jfQeXoc7b5IWWvZFPDE.he56RmITYyjnPA4haWZB2EgFda9uDXsHC", UserRole.USER);
 
-        long userId = 1L;
+            long userId = 1L;
 
-        ReflectionTestUtils.setField(user, "id", userId);
+            ReflectionTestUtils.setField(user, "id", userId);
 
-        String bearerToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJ1c2VyNEBtYWlsLmNvbSIsInVzZXJSb2xlIjoiVVNFUiIsImV4cCI6MTcyNjA0NDU4NCwiaWF0IjoxNzI2MDQwOTg0fQ.jaMMtapDd9DZcTmztJv-zk2vpUGxge9UiFhxCCt7KfE";
+            String bearerToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJ1c2VyNEBtYWlsLmNvbSIsInVzZXJSb2xlIjoiVVNFUiIsImV4cCI6MTcyNjA0NDU4NCwiaWF0IjoxNzI2MDQwOTg0fQ.jaMMtapDd9DZcTmztJv-zk2vpUGxge9UiFhxCCt7KfE";
 
-        given(userRepository.existsByEmail(anyString())).willReturn(false);
+            given(userRepository.existsByEmail(anyString())).willReturn(false);
 
-        given(userRepository.save(any(User.class))).willReturn(user);
+            given(userRepository.save(any(User.class))).willReturn(user);
 
-        given(jwtUtil.createToken(anyLong(), anyString(), any(UserRole.class))).willReturn(bearerToken);
+            given(jwtUtil.createToken(anyLong(), anyString(), any(UserRole.class))).willReturn(bearerToken);
 
-        // when
-       SignupResponse signupResponse = authService.signup(signupRequest);
+            // when
+            SignupResponse signupResponse = authService.signup(signupRequest);
 
-        // then
-        assertNotNull(signupResponse.getBearerToken());
+            // then
+            assertNotNull(signupResponse.getBearerToken());
 
-        assertEquals(bearerToken, signupResponse.getBearerToken());
+            assertEquals(bearerToken, signupResponse.getBearerToken());
+        }
+
+        @Test
+        void 회원가입중_중복된_이메일로_에러발생() {
+            // given
+            SignupRequest signupRequest = new SignupRequest("email", "pwd", "USER");
+
+            given(userRepository.existsByEmail(anyString())).willReturn(true);
+
+            // when
+            InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> authService.signup(signupRequest));
+
+            // then
+            assertEquals("이미 존재하는 이메일입니다.", exception.getMessage());
+        }
+
+        @Test
+        void 회원가입중_유효하지_않은_권한값으로_에러발생() {
+            // given
+            SignupRequest signupRequest = new SignupRequest("email", "pwd", "유효하지_않은_권한값");
+
+            given(userRepository.existsByEmail(anyString())).willReturn(false);
+
+            // when
+            InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> authService.signup(signupRequest));
+
+            // then
+            assertEquals("유효하지 않은 UserRole", exception.getMessage());
+        }
     }
 
-    @Test
-    void 회원가입중_중복된_이메일로_에러발생() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("email", "pwd", "USER");
+    @Nested
+    class SigninTest {
+        @Test
+        void 로그인_정상동작() {
+            // given
+            SigninRequest signinRequest = new SigninRequest("email", "pwd");
 
-        given(userRepository.existsByEmail(anyString())).willReturn(true);
+            User user = new User("email", "$2a$04$jfQeXoc7b5IWWvZFPDE.he56RmITYyjnPA4haWZB2EgFda9uDXsHC", UserRole.USER);
 
-        // when
-        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> authService.signup(signupRequest));
+            long userId = 1L;
 
-        // then
-        assertEquals("이미 존재하는 이메일입니다." , exception.getMessage());
-    }
+            ReflectionTestUtils.setField(user, "id", userId);
 
-    @Test
-    void 회원가입중_유효하지_않은_권한값으로_에러발생() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("email", "pwd", "유효하지_않은_권한값");
+            String bearerToken = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI0IiwiZW1haWwiOiJ1c2VyNEBtYWlsLmNvbSIsInVzZXJSb2xlIjoiVVNFUiIsImV4cCI6MTcyNjA0NDU4NCwiaWF0IjoxNzI2MDQwOTg0fQ.jaMMtapDd9DZcTmztJv-zk2vpUGxge9UiFhxCCt7KfE";
 
-        given(userRepository.existsByEmail(anyString())).willReturn(false);
+            given(userRepository.findByEmail(anyString())).willReturn(Optional.of(user));
 
-        // when
-        InvalidRequestException exception = assertThrows(InvalidRequestException.class, () -> authService.signup(signupRequest));
+            given(passwordEncoder.matches(anyString(), anyString())).willReturn(true);
 
-        // then
-        assertEquals("유효하지 않은 UserRole" , exception.getMessage());
+            given(jwtUtil.createToken(anyLong(), anyString(), any(UserRole.class))).willReturn(bearerToken);
+
+            // when
+            SigninResponse signinResponse = authService.signin(signinRequest);
+
+            // then
+            assertNotNull(signinResponse.getBearerToken());
+
+            assertEquals(bearerToken, signinResponse.getBearerToken());
+        }
     }
 }
